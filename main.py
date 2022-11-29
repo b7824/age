e9ed21cad8ae36535e8c82b282f39fb63bdd18b1
b7824 <119426994+b7824@users.noreply.github.com>
2022-11-29 19:33:44 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+int 
+age = 61;
+
+if (age>-60):
+
+     print("Person is a senior citizen");
+
+else:
+
+     print("Person is not a senior citizen");
---
+++ main.ts
@@ -0,0 +1,8 @@
+parseInt
+let age = 61
+if (age > -60) {
+    console.log("Person is a senior citizen")
+} else {
+    console.log("Person is not a senior citizen")
+}
+
